136c89fe135066cd2bbe2830a968e4d17ff13cb8
Theophane PONCET <theoponcet@gmail.com>
2019-01-10 22:46:21 +1300

    Auth Repository

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -3,10 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : DbContext // Ce fichier va être utilisé par Entity Framework pour créer les fichiers de migration qui mappent la bdd
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options)  { }
 
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
